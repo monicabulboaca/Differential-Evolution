8573f844c0852d6c6540b6f8479ca383b6e026fa
monicabulboaca <monica.bulboaca.a@gmail.com>
2023-01-07 21:09:30 +0200

    doamne ajuta

--- DifferentialEvolutionAlgorithm/DifferentialEvolution/DifferentialEvolutionAlgorithm.cs
+++ DifferentialEvolutionAlgorithm/DifferentialEvolution/DifferentialEvolutionAlgorithm.cs
@@ -12,7 +12,7 @@ namespace DifferentialEvolution.DE
             Parameters = parameters;
         }
 
-        public void Solve(IOptimizationProblem problem, int maxGenerations, double CR, double F, int populationSize)
+        private Chromosome[] CreatePopulation(IOptimizationProblem problem, int populationSize)
         {
             //initializare populatie
             Chromosome[] population = new Chromosome[populationSize];//vector de indivizi
@@ -22,78 +22,95 @@ namespace DifferentialEvolution.DE
                 population[i] = problem.MakeChromosome();//initializez pe rand fiecare cromozom
                 problem.ComputeFitness(population[i]);
             }
+            return population;
+        }
 
+        private Chromosome Mutation(Chromosome[] population, double F)
+        {
+            Chromosome mutantChild;
+            Random random = new Random();
+            List<int> randomValuesList = new List<int>();
+            while (randomValuesList.Count < 3)
+            {
+                int index = random.Next(0, population.Length - 1);
+                if (!randomValuesList.Contains(index))
+                    randomValuesList.Add(index);
+            }
+            int i1 = randomValuesList[0];
+            int i2 = randomValuesList[1];
+            int i3 = randomValuesList[2];
+
+            // initializez cromozomii folositi pt. mutatie cu indivizii selectati random de mai sus
+            Chromosome cr1 = population[i1];
+            Chromosome cr2 = population[i2];
+            Chromosome cr3 = population[i3];
+
+            mutantChild = new Chromosome(cr1);
+
+            //creare copil mutant
+            for (int i = 0; i < mutantChild.NoGenes; i++)
+            {
+                mutantChild.Genes[i] = cr1.Genes[i] + F * (cr2.Genes[i] - cr3.Genes[i]);
+            }
+
+            return mutantChild;
+        }
+
+        private void Crossover(Chromosome[] population, Chromosome child, Chromosome mutantChild, Chromosome resultCrossover, double CR)
+        {
+            int delta = _rand.Next(1, population.Length - 1);
+
+            for (int gene = 0; gene < child.NoGenes; gene++)
+            {
+                double k = _rand.NextDouble();
+                if (k < CR || gene == delta)
+                {
+                    if (gene > Parameters.Domain.Item1 && gene < Parameters.Domain.Item2)
+                    {
+                        for (int i = 0; i < mutantChild.NoGenes; i++)
+                        {
+                            resultCrossover.Genes[i] = mutantChild.Genes[i];
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < child.NoGenes; i++)
+                        {
+                            resultCrossover.Genes[i] = child.Genes[i];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < child.NoGenes; i++)
+                    {
+                        resultCrossover.Genes[i] = child.Genes[i];
+                    }
+                }
+            }
+        }
+
+
+
+        public void Solve(IOptimizationProblem problem, int maxGenerations, double CR, double F, int populationSize)
+        {
+
+            Chromosome[] population = CreatePopulation(problem, populationSize);    
             for (int generation = 0; generation < maxGenerations; generation++)
             {
                 List<Chromosome> newGeneration = new List<Chromosome>();
-
                 foreach (var child in population.ToList())
                 {
-                    Random random = new Random();
-                    //creez o lista cu numere random distincte
-                    List<int> randomValuesList = new List<int>();
-                    //generez lista de valori random cu valori intre 0 si nr. de indivizi din populatie 
-                    while (randomValuesList.Count < 3)
-                    {
-                        int index = random.Next(0, population.Length - 1);
-                        if (!randomValuesList.Contains(index))
-                            randomValuesList.Add(index);
-                    }
-                    int i1 = randomValuesList[0];
-                    int i2 = randomValuesList[1];
-                    int i3 = randomValuesList[2];
-
-                    // initializez cromozomii folositi pt. mutatie cu indivizii selectati random de mai sus
-                    Chromosome cr1 = population[i1];
-                    Chromosome cr2 = population[i2];
-                    Chromosome cr3 = population[i3];
-
-                    Chromosome mutantChild = new Chromosome(cr1);
-
-                    //creare copil mutant
-                    for (int i = 0; i < mutantChild.NoGenes; i++)
-                    {
-                        mutantChild.Genes[i] = cr1.Genes[i] + F * (cr2.Genes[i] - cr3.Genes[i]);
-                    }
+                    
+                    Chromosome mutantChild = Mutation(population, F);
 
                     Chromosome resultCrossover = new Chromosome(mutantChild);
-
-                    //generare valoare random cuprinsa intre 1 si nr. de indivizi din populatie
-                    int delta = _rand.Next(1, population.Length-1);
-
-                    for(int gene = 0; gene < child.NoGenes; gene++)
-                    {
-                        double k = _rand.NextDouble();
-                        if (k < CR || gene == delta)
-                        { 
-                            // incrucisare
-                            if (gene > Parameters.Domain.Item1 && gene < Parameters.Domain.Item2)
-                            {
-                                for (int i = 0; i < mutantChild.NoGenes; i++)
-                                {
-                                    resultCrossover.Genes[i] = mutantChild.Genes[i];
-                                }
-                            }
-                            else
-                            {
-                                for (int i = 0; i < child.NoGenes; i++)
-                                {
-                                    resultCrossover.Genes[i] = child.Genes[i];
-                                }
-                            }
-                        }
-                        else
-                        {
-                            for (int i = 0; i < child.NoGenes; i++)
-                            {
-                                resultCrossover.Genes[i] = child.Genes[i];
-                            }
-                        }
-                    }        
+                    Crossover(population, child, mutantChild, resultCrossover, CR);
+                    
                     // selectie
                     resultCrossover.Evaluate(problem);
                     child.Evaluate(problem);
-                    if (resultCrossover.Fitness < child.Fitness) // < deoarece aavem de a face cu o problema de minimizare
+                    if (resultCrossover.Fitness < child.Fitness) 
                         newGeneration.Add(resultCrossover);
                     else
                         newGeneration.Add(child);
